f8264552de5476d7e61b787ab2e49f48868f88c3
y-onee <saayonee05@gmail.com>
2025-10-28 15:33:16 -0300

    first failed test

---
+++ main/java/UserDB.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class UserDB {
+    void get(User user);
+
+
+}
--- test/java/TaskTest.java
+++ test/java/TaskTest.java
@@ -1,8 +1,21 @@
 package test.java;
+import main.java.UserDB;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.mock.MockName;
+import org.mockito.Mock;
 
 public class TaskTest {
+
     @Test
-    public void testTask() {}
+    public void assignToTest(String team_member) {
+        UserDB userDB = new UserDB();
+
+        String member = userDB.getUser(1);
+
+        assert(assignTo(member));
+
+    }
  
 }
