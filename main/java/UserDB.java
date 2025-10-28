c979b9abcf35c71257a99aee62901b9d02d914f4
y-onee <saayonee05@gmail.com>
2025-10-28 15:38:36 -0300

    Give task to member

--- main/java/UserDB.java
+++ main/java/UserDB.java
@@ -1,12 +1,25 @@
 package main.java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserDB {
     public String getUser(){
         String userName = "member1";
         return userName;
     };
 
+    public String getTask(){
+        String task = "task1";
+        return task;
+    }
+
     public boolean assignTo(String member){
+        Map<String, String> taskList = new HashMap<String, String>();
+        taskList.put(getUser(), getTask());
+        if(taskList.isEmpty()) {
+            return false;
+        };
         return true;
     }
 
